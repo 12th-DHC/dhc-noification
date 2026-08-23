f91cd859614268dd72c24612f0b44e53e2993e7d
JANG SEONG JU <printhelloworld0702@gmail.com>
2026-08-24 00:36:05 +0900

    fix: 사용하지 않는 import 문 제거

--- src/main/java/com/sungjujjang/dhc/notification/global/RabbitMQConfig.java
+++ src/main/java/com/sungjujjang/dhc/notification/global/RabbitMQConfig.java
@@ -1,11 +1,9 @@
 package com.sungjujjang.dhc.notification.global;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
